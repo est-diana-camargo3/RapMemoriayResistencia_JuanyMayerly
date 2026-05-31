676c790f0ff89661701691429f2b0c39d8b3b047
MayeCamargo <est.diana.camargo3@unimilitar.edu.co>
2026-05-30 22:09:01 -0500

    pantalla final del periodico exitosa

--- RapMemoria3/Assets/Scripts/FinalRecorrido.cs
+++ RapMemoria3/Assets/Scripts/FinalRecorrido.cs
@@ -3,32 +3,11 @@ using UnityEngine.SceneManagement;
 
 public class FinalRecorrido : MonoBehaviour
 {
-    [Header("Panel Final")]
-    public GameObject panelFinal;
-
-    private bool activated = false;
-
-    void Start()
-    {
-        panelFinal.SetActive(false);
-    }
-
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player") && !activated)
+        if (other.CompareTag("Player"))
         {
-            activated = true;
-
-            panelFinal.SetActive(true);
-
-            Invoke(nameof(VolverMenu), 5f);
+            SceneManager.LoadScene("04_Final");
         }
-    }
-
-    void VolverMenu()
-    {
-        Time.timeScale = 1f;
-
-        SceneManager.LoadScene("00_MenuInicio");
     }
 }
